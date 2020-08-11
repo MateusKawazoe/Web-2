faf8ecda190c22bf912afc58813a1d522aa7db42
Mateuskwz <mateus.takeshi@ykt.com.br>
2020-08-11 18:15:22 -0300

    Com loading agora

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.botao').on('click', async function() {
+        $(".imagem").attr('src', "src/assets/loading.gif")
         const response = await axios.get('https://api.thecatapi.com/v1/images/search')
         $(".imagem").attr('src', response.data[0].url)
     })
