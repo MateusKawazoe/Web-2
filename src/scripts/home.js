d0bdc5b9c8e3110d9bd4860921587c86f29bc364
Mateuskwz <mateus.takeshi@ykt.com.br>
2020-08-11 18:21:08 -0300

    Loading novo

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.botao').on('click', async function() {
-        $(".imagem").attr('src', "src/assets/loading.gif")
+        $(".imagem").attr('src', "https://flevix.com/wp-content/uploads/2019/07/Curve-Loading.gif")
         const response = await axios.get('https://api.thecatapi.com/v1/images/search')
         $(".imagem").attr('src', response.data[0].url)
     })
