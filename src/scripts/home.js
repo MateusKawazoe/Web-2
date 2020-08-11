11a6187f4d02041fb9981d3230df2a8959ab389a
Mateus Takeshi Kawazoe <34221747+MateusKawazoe@users.noreply.github.com>
2020-08-11 18:23:08 -0300

    Update home.js

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.botao').on('click', async function() {
-        $(".imagem").attr('src', "https://flevix.com/wp-content/uploads/2019/07/Curve-Loading.gif")
+        $(".imagem").attr('src', "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/6d391369321565.5b7d0d570e829.gif")
         const response = await axios.get('https://api.thecatapi.com/v1/images/search')
         $(".imagem").attr('src', response.data[0].url)
     })
