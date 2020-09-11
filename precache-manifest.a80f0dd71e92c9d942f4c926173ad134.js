157324a286d83fb8cc012709061ef748fa341268
Mateuskwz <mateus.takeshi@ykt.com.br>
2020-09-10 21:41:21 -0300

    Updates

---
+++ precache-manifest.a80f0dd71e92c9d942f4c926173ad134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8931497d6eaec3d8eba81dba76166801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5177e47a7eb5ae21dc5",
+    "url": "/static/css/main.caedfc03.chunk.css"
+  },
+  {
+    "revision": "dd0852e3bff45e074e60",
+    "url": "/static/js/2.194134db.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.194134db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5177e47a7eb5ae21dc5",
+    "url": "/static/js/main.48ba9975.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "f5b64330fdb3e8513a42bae98eb0046a",
+    "url": "/static/media/logo.f5b64330.png"
+  }
+]);
